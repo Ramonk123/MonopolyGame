cf9aa340a1222a29ec61d4548b4f45a1f287b6a1
RamonK <s1127893@student.hsleiden.nl>
2021-06-03 16:00:53 +0200

    extended met Application voor JavaFX

--- src/main/java/Monopoly/Monopoly.java
+++ src/main/java/Monopoly/Monopoly.java
@@ -1,8 +1,15 @@
 package Monopoly;
 
-public class Monopoly {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Monopoly extends Application {
 
     public static void main(String[] args) {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
     }
 }
