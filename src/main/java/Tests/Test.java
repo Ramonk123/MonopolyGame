a81511c97372d27ebdb340b7390602254495fc17
c9d5e1 <47698642+c9d5e1@users.noreply.github.com>
2021-06-04 15:57:43 +0200

    tests

---
+++ src/main/java/Tests/Test.java
@@ -0,0 +1,12 @@
+package Tests;
+
+// this no work. no bueno
+public class Test {
+    /*public static void main(String[] args) {
+        Runnable runnable = Test::foo;
+    }
+
+    static void foo(i) {
+        System.out.println(i);
+    }*/
+}
