fddd00da43759bda6048689c20d10dd07e6ed909
c9d5e1 <47698642+c9d5e1@users.noreply.github.com>
2021-06-04 14:04:09 +0200

    player model

--- src/main/java/Controllers/AuctionController.java
+++ src/main/java/Controllers/AuctionController.java
@@ -1,4 +1,21 @@
 package Controllers;
 
+import Views.View;
+import com.google.cloud.firestore.DocumentSnapshot;
+
 public class AuctionController implements Controller {
+    @Override
+    public void registerObserver(View v) {
+
+    }
+
+    @Override
+    public void unregisterObserver(View v) {
+
+    }
+
+    @Override
+    public void notifyObservers(DocumentSnapshot ds) {
+
+    }
 }
--- src/main/java/Models/Player.java
+++ src/main/java/Models/Player.java
@@ -1,4 +1,37 @@
 package Models;
 
-public class Player implements Model {
+import ObserveablePattern.Observer;
+import Views.View;
+import com.google.cloud.firestore.DocumentSnapshot;
+
+import java.util.List;
+
+public class Player implements Model, Position {
+    private String name;
+    private String pawn_icon;
+    private Wallet wallet;
+    private int position;
+    private List<View> views;
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public void registerObserver(View v) {
+        views.add(v);
+    }
+
+    @Override
+    public void unregisterObserver(View v) {
+        views.remove(v);
+    }
+
+    @Override
+    public void notifyObservers(DocumentSnapshot ds) {
+        for (View v : views) {
+            v.update(ds);
+        }
+    }
 }
