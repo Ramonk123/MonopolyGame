eaf58cbcdca71ac17104422bac7c3ac41770c2be
Brandon Plokker <brandonplokker@live.nl>
2021-06-04 15:34:03 +0200

    added the firestore class functions to be used in andere classes.! :)

--- src/main/java/Firestore/Firestore.java
+++ src/main/java/Firestore/Firestore.java
@@ -2,20 +2,22 @@ package Firestore;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Firestore {
 
-
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+    public com.google.cloud.firestore.Firestore getdb() throws IOException {
 
         FileInputStream serviceAccount =
                 new FileInputStream("monopolyteam6-47708-firebase-adminsdk-tp5m6-9a482af7f7.json");
@@ -30,21 +32,24 @@ public class Firestore {
 
         Firestore firestore = new Firestore();
 
-        firestore.getQuoteFromFirestore(db);
+        return db;
     }
 
-    public void getQuoteFromFirestore(com.google.cloud.firestore.Firestore db) throws InterruptedException, ExecutionException {
+    // methods you can use to upload data to firestore.
+    //Exceptions are for the last line in this function, update time.
+    public void exampleUpload(com.google.cloud.firestore.Firestore db) throws ExecutionException, InterruptedException {
+        // for regular vars like string int and all that good stuff.
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("titelkop", "texthierinofzo");
 
-        DocumentReference docRef = db.collection("sampleData").document("my_new_document");
-        ApiFuture<DocumentSnapshot> future = docRef.get();
-        DocumentSnapshot document = future.get();
+        ArrayList<Object> arrayExample = new ArrayList<>();
+        Collections.addAll(arrayExample, 12, 32, "hello world", false);
+        docData.put("arrayExample", arrayExample);
 
-        if(document.exists()) {
-            String quote = (String) document.get("Gandhi");
-            System.out.println(quote);
-        }else {
-            System.out.println("No such document :(");
-        }
+        ApiFuture<WriteResult> future = db.collection("dataplace").document("submap").set(docData);
+        // docData is the data you upload of course.
+
+        //only use this for testing, print Update time to console
+        System.out.println("Update time: " + future.get().getUpdateTime());
     }
-
 }
