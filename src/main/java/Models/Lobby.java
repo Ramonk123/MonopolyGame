3cff66539856275bb13ef7a2c257eb2ce80109d9
c9d5e1 <47698642+c9d5e1@users.noreply.github.com>
2021-06-07 12:04:58 +0200

    last updates for new changes

--- src/main/java/Models/Lobby.java
+++ src/main/java/Models/Lobby.java
@@ -1,11 +1,36 @@
 package Models;
 
+import ObserveablePattern.Observer;
+import Views.LobbySubject;
 import Views.View;
 import com.google.cloud.firestore.DocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lobby implements Model {
+public class Lobby implements Model, LobbySubject, Observer<DocumentSnapshot> {
+    private List<Observer<LobbySubject>> observers = new ArrayList<>();
 
+    @Override
+    public void registerObserver(Observer<LobbySubject> o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void unregisterObserver(Observer<LobbySubject> o) {
+        
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer<LobbySubject> o : observers) {
+            o.update(this);
+        }
+    }
+
+    @Override
+    public void update(final DocumentSnapshot state) {
+        // perform updates on this model
+        notifyObservers();
+    }
 }
