cb3461bd257437964c6cee3ba775df686b7f1e70
c9d5e1 <47698642+c9d5e1@users.noreply.github.com>
2021-06-07 16:28:22 +0200

    Actions class. UUID class.

---
+++ src/main/java/Models/Actions.java
@@ -0,0 +1,7 @@
+package Models;
+
+public class Actions {
+    public static void exampleAction(Player p) {
+        System.out.println("An example action has been performed.");
+    }
+}
--- src/main/java/Monopoly/UUID.java
+++ src/main/java/Monopoly/UUID.java
@@ -4,13 +4,21 @@ import java.util.HashMap;
 
 public class UUID {
     private static HashMap<String, Boolean> cache = new HashMap<>();
+    private String id;
 
     public UUID(String id) {
         if (cache.get(id) == null) {
             cache.put(id, true);
+            this.id = id;
         } else {
             System.out.println(String.format("UUID '%s' already exists.", id));
             System.exit(69420);
         }
+    }
+
+    public String getId() { return id; }
+
+    public static boolean compare(UUID idOne, UUID idTwo) {
+        return idOne == idTwo;
     }
 }
