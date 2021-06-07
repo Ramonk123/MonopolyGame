9da62c1b1413d3e40dd634bb1ffe22f847371118
Brandon Plokker <brandonplokker@live.nl>
2021-06-07 13:40:40 +0200

    card getters and setters, raw need fix.

--- src/main/java/Models/Card.java
+++ src/main/java/Models/Card.java
@@ -1,4 +1,42 @@
 package Models;
 
-public abstract class Card implements Model {
+import java.util.function.Consumer;
+
+public class Card implements Model {
+
+    public Card(String title, String description, Consumer action) {
+        this.title = title;
+        this.description = description;
+        this.action = action;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Consumer getAction() {
+        return action;
+    }
+
+    public void setAction(Consumer action) {
+        this.action = action;
+    }
+
+    private String title;
+    private String description;
+    private Consumer action;
+
+
 }
