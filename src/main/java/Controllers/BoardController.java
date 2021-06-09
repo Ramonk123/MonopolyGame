533da7b2ec637317ac3a59877462eed3b8d6b0ff
VinSe <vincentmcmaps@gmail.com>
2021-06-09 22:03:48 +0200

    Added methods that will put the names of the players in the labels

--- src/main/java/Controllers/BoardController.java
+++ src/main/java/Controllers/BoardController.java
@@ -1,13 +1,20 @@
 package Controllers;
 
 import Models.Board;
+import Models.Player;
 import Models.Wallet;
 import ObserveablePattern.Observer;
 import ObserveablePattern.Subject;
 import Views.HasStage;
 import Views.View;
 import com.google.cloud.firestore.DocumentSnapshot;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class BoardController implements Controller, Subject<DocumentSnapshot>, HasStage {
 
@@ -17,6 +24,23 @@ public class BoardController implements Controller, Subject<DocumentSnapshot>, H
     public BoardController() {
         board = new Board();
     }
+
+    @FXML
+    private Label BoardViewUsername1Label;
+    @FXML
+    private Label BoardViewUsername2Label;
+    @FXML
+    private Label BoardViewUsername3Label;
+    @FXML
+    private Label BoardViewUsername4Label;
+    @FXML
+    private Label BoardViewUsername5Label;
+    @FXML
+    private Label BoardViewUsername6Label;
+    @FXML
+    private Label BoardViewUsername7Label;
+    @FXML
+    private Label BoardViewUsername8Label;
 
     @Override
     public void registerObserver(Observer<DocumentSnapshot> observer) { }
@@ -31,6 +55,53 @@ public class BoardController implements Controller, Subject<DocumentSnapshot>, H
 
     @Override
     public void setStage(Stage primaryStage) {
+        board.setStage(primaryStage);
+    }
 
+    public void setPlayersNamesToLabels() {
+        PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
+        ArrayList<Player> players = playerController.getPlayers();
+        for(int i = 0; i < 8; i++) {
+            try {
+                setPlayerNameToLabel(players.get(i).getName(), i + 1);
+            } catch(IndexOutOfBoundsException indexOutOfBoundsException) {
+                setVisibilityLabel(i + 1);
+            }
+        }
+    }
+
+    private void setPlayerNameToLabel(String name, int labelNumber) {
+        Label label = getLabelByNumber(labelNumber);
+        label.setText(name);
+    }
+
+    private void setVisibilityLabel(int labelNumber) {
+        Label label = getLabelByNumber(labelNumber);
+        label.setVisible(false);
+    }
+
+    @Nullable
+    private Label getLabelByNumber(int labelNumber) {
+        Label label = null;
+
+        switch(labelNumber) {
+            case 1:
+                label = BoardViewUsername1Label;
+            case 2:
+                label = BoardViewUsername2Label;
+            case 3:
+                label = BoardViewUsername3Label;
+            case 4:
+                label = BoardViewUsername4Label;
+            case 5:
+                label = BoardViewUsername5Label;
+            case 6:
+                label = BoardViewUsername6Label;
+            case 7:
+                label = BoardViewUsername7Label;
+            case 8:
+                label = BoardViewUsername8Label;
+        }
+        return label;
     }
 }
