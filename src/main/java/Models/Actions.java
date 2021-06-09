a754fc2a536c45d066eab85619d3c53123717c82
RamonK <s1127893@student.hsleiden.nl>
2021-06-09 15:48:30 +0200

    Added most card actions possible. Check TODO for missing items

--- src/main/java/Models/Actions.java
+++ src/main/java/Models/Actions.java
@@ -4,6 +4,7 @@ import Controllers.ControllerRegistry;
 import Controllers.MainMenuController;
 import Controllers.PlayerController;
 import Controllers.TransactionController;
+import Monopoly.UUID;
 
 public class Actions {
 
@@ -12,11 +13,8 @@ public class Actions {
     }
 
     public static void teleportToLocation(Player player, int index) {
-         int BOARDWALK_INDEX = 40; //TODO: Set right index for location
-
-
         PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
-        playerController.teleportTo(player, BOARDWALK_INDEX);
+        playerController.teleportTo(player, index);
     }
 
     public static void teleportToNearestRailroad(Player player) {
@@ -34,7 +32,16 @@ public class Actions {
     public static void receiveFunds(Player player, int amount) {
         TransactionController transactionController = (TransactionController) ControllerRegistry.get(TransactionController.class);
         try{
-            transactionController.addBalance(player.getId(), 50);
+            transactionController.addBalance(player.getPlayersEnum(), amount);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void payFunds(Player player, int amount) {
+        TransactionController transactionController = (TransactionController) ControllerRegistry.get(TransactionController.class);
+        try{
+            transactionController.subtractBalance(player.getPlayersEnum(), amount);
         } catch(Exception e) {
             System.out.println(e);
         }
@@ -42,6 +49,37 @@ public class Actions {
 
     public static void goBackThreeSpaces(Player player) {
         PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
+        try{playerController.movePlayer(player.getName(),-3);}
+        catch(Exception e) {System.out.println(e);
+        }
+    }
 
+    public static void payEachPlayer(Player player) {
+        TransactionController transactionController = (TransactionController) ControllerRegistry.get(TransactionController.class);
+        PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
+
+        for(Player eachPlayer: playerController.getPlayers()) {
+            try {
+                if(!UUID.compare(player,eachPlayer))
+                transactionController.payBalance(player.getPlayersEnum(), eachPlayer.getPlayersEnum(), 50);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void receiveFromEachPlayer(Player player) {
+        TransactionController transactionController = (TransactionController) ControllerRegistry.get(TransactionController.class);
+        PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
+
+        for(Player eachPlayer: playerController.getPlayers()) {
+            try {
+                if(!UUID.compare(player,eachPlayer))
+                    transactionController.payBalance(eachPlayer.getPlayersEnum(),player.getPlayersEnum(),10);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
