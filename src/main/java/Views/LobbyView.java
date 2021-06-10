7d1630cd4964f03b0b3cc5cfb84b1c544c1b30fe
RamonK <s1127893@student.hsleiden.nl>
2021-06-10 17:13:10 +0200

    Added method that will update player names in Lobby view

--- src/main/java/Views/LobbyView.java
+++ src/main/java/Views/LobbyView.java
@@ -3,20 +3,26 @@ package Views;
 import Controllers.ControllerRegistry;
 import Controllers.LobbyController;
 import Controllers.MainMenuController;
+import Controllers.PlayerController;
+import Models.Player;
 import ObserveablePattern.Observer;
 import com.google.cloud.firestore.DocumentSnapshot;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class LobbyView implements View, Observer<LobbySubject>, HasStage {
     //Screensize
-    int WIDTH = 600;
-    int HEIGHT = 400;
-
+    int WIDTH = 630;
+    int HEIGHT = 600;
     private Stage primaryStage;
 
     public LobbyView() {
@@ -29,6 +35,7 @@ public class LobbyView implements View, Observer<LobbySubject>, HasStage {
         Parent root = loader.load();
 
         primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -44,7 +51,25 @@ public class LobbyView implements View, Observer<LobbySubject>, HasStage {
 
     @Override
     public void update(LobbySubject state) {
+        List<Player> players = state.getPlayers();
+        updateLobbyView();
 
     }
+    //This function will check for new players joining lobby and updating player names
+    public void updateLobbyView() {
+
+        LobbyController lobbyController = (LobbyController) ControllerRegistry.get(LobbyController.class);
+        PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
+
+        ArrayList<Label> labelList = lobbyController.getUserLabelList();
+        lobbyController.getTokenLabel().setText(String.valueOf(lobbyController.getToken()));
+
+        int playersJoined = playerController.getPlayers().size();
+        for(int i = 0; i < playersJoined; i++) {
+            labelList.get(i).setText(playerController.getPlayers().get(i).getName());
+        }
+    }
+
+
 
 }
