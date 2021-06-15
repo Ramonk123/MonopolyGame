6477896205195eb605774b10daede4f8c11e4686
VinSe <vincentmcmaps@gmail.com>
2021-06-15 16:17:28 +0200

    Added RollDice & movePlayer method

--- src/main/java/Controllers/TurnController.java
+++ src/main/java/Controllers/TurnController.java
@@ -4,6 +4,7 @@ import Firestore.FirestoreFormattable;
 import Models.Player;
 import Models.Throw;
 import Models.Turn;
+import Monopoly.UUID;
 import ObserveablePattern.Observer;
 import ObserveablePattern.Subject;
 import Resetter.Resettable;
@@ -34,11 +35,6 @@ public class TurnController
         return turn.getCurrentPlayer();
     }
 
-    public Throw getCurrentThrow() {
-        ThrowController throwController = (ThrowController) ControllerRegistry.get(ThrowController.class);
-        return throwController.getCurrentThrow();
-    }
-
     @Override
     public Object getFirestoreFormat() {
         //Map<String, Object> map = new HashMap<>();
@@ -62,13 +58,55 @@ public class TurnController
     }
 
     @Override
-    public void registerObserver(Observer<DocumentSnapshot> observer) { }
+    public void registerObserver(Observer<DocumentSnapshot> observer) {
+    }
 
     @Override
-    public void unregisterObserver(Observer<DocumentSnapshot> observer) { }
+    public void unregisterObserver(Observer<DocumentSnapshot> observer) {
+    }
 
     @Override
     public void notifyObservers() {
         turn.update(documentSnapshot);
+    }
+
+    public void RollDice() throws PlayerException {
+        Players currentPlayerEnum = getCurrentPlayer();
+
+        PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
+        Players clientPlayerEnum = playerController.getClientPlayersEnum();
+
+        if(UUID.compare(currentPlayerEnum, clientPlayerEnum)) {
+            ThrowController throwController = (ThrowController) ControllerRegistry.get(ThrowController.class);
+            throwController.throwDice();
+            int amountThrown = throwController.getTotalEyes();
+            int thrownDouble = 0;
+            if(throwController.isDouble()) {
+                thrownDouble++;
+                while(throwController.isDouble()) {
+                    if(thrownDouble >= 3) {
+                        //TODO: Go to Jail
+                        break;
+                    }
+                    throwController.throwDice();
+                    amountThrown += throwController.getTotalEyes();
+                    thrownDouble++;
+                }
+            }
+            movePlayer(currentPlayerEnum, amountThrown);
+        }
+
+    }
+
+    public void movePlayer(Players currentPlayerEnum, int amountThrown) throws PlayerException {
+        Player currentPlayer = ((PlayerController) ControllerRegistry.get(PlayerController.class)).getPlayerByPlayersEnum(currentPlayerEnum).orElseThrow(() -> new PlayerException("Player NOT Found"));
+
+        int oldPlayerPosition = currentPlayer.getPosition();
+        currentPlayer.movePlayer(amountThrown);
+        int newPlayerPosition = currentPlayer.getPosition();
+
+        BoardController boardController = (BoardController) ControllerRegistry.get(BoardController.class);
+        boardController.movePlayerOnBoard(currentPlayerEnum, oldPlayerPosition, newPlayerPosition);
+
     }
 }
