23f9d2bc5b28bd038a53febe6a61a718458db701
VinSe <vincentmcmaps@gmail.com>
2021-06-16 16:22:59 +0200

    Added Javadocs to the Exceptions

--- src/main/java/Exceptions/PlayerException.java
+++ src/main/java/Exceptions/PlayerException.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * Exception for when things are going wrong with the player.
+ */
 public class PlayerException extends Exception {
     public PlayerException(String errorMessage) {
         super(errorMessage);
--- src/main/java/Exceptions/TransactionException.java
+++ src/main/java/Exceptions/TransactionException.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * Exception for when things are going wrong with a transaction.
+ */
 public class TransactionException extends Exception {
     public TransactionException(String errorMessage) {
         super(errorMessage);
