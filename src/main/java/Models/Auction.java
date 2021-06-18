e39c3a73add007c6f100d95d299f1350bb97795a
Brandon <brrandonplokker@live.nl>
2021-06-19 01:19:24 +0200

    Model voor Auction gemaakt. Onze oude plan voor action was dat iedereen blind ging bieden, dit is echter niet hoe het in de originele monopolio gaat en vandaar dat ik het ga uitbreiden, waarschijnlijk veel vergeten in de model want het is 1 uur en ik heb dit net bedacht pas.

--- src/main/java/Models/Auction.java
+++ src/main/java/Models/Auction.java
@@ -1,8 +1,13 @@
 package Models;
 
 import Controllers.AuctionController;
+import Controllers.FireStoreController;
+import Controllers.PlayerController;
 import Controllers.Players;
+import Firestore.FirestoreFormattable;
+import Monopoly.UUID;
 import ObserveablePattern.Observer;
+import Views.BoardView;
 import Views.View;
 import com.google.cloud.firestore.DocumentSnapshot;
 
@@ -10,18 +15,30 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Model for the Auction.
  */
-public class Auction implements Model {
-    private Player player;
+public class Auction implements Model, Observer<DocumentSnapshot>, FirestoreFormattable {
     private boolean startedAuction = false;
-    private HashMap<Players, Integer> playerBids = new HashMap<>();
+    private HashMap<String, Object> playerBids = new HashMap<>();
+    private HashMap<String, Object> auction= new HashMap<>();
+    private String locationID;
+    private String seller;
+
+
+    public void startAuction(String locationID, String seller) {
+        this.locationID = locationID;
+        this.seller = seller;
+        this.startedAuction = true;
+    }
 
     public void addPlayerBid(int bid) {
-        //playerBidCollection.add(playerBidInfo.put(player,bid));
-
+        PlayerController playerController = new PlayerController();
+        playerBids.put("player_uuid", playerController.getClientPlayersEnum().getId().getId());
+        playerBids.put("bid", bid);
+        auction.put("playerBids", playerBids);
 
         //TODO: Create method to send playerBidCollection to Firestore
     }
@@ -30,8 +47,39 @@ public class Auction implements Model {
         return startedAuction;
     }
 
-    public void setStartedAuction() {
-        this.startedAuction = true;
+    @Override
+    public Object getFirestoreFormat() {
+        return auction;
+    }
+
+    @Override
+    public void update(DocumentSnapshot documentSnapshot) {
+        System.out.println("AUCTIONwow");
+        HashMap<String, Object> firestoreAuction = (HashMap<String, Object>) documentSnapshot.get("auction");
+        if(firestoreAuction == null){
+            startedAuction = false;
+        }else{
+            System.out.println(firestoreAuction);
+            startedAuction = true;
+            playerBids = (HashMap<String, Object>) firestoreAuction.get("playerBids");
+            locationID = (String) firestoreAuction.get("location");
+        }
+    }
+
+    public String getLocationID(){
+        return locationID;
+    }
+
+    public HashMap<String, Object> getPlayerBids() {
+        return playerBids;
+    }
+
+    public void setPlayerBids(HashMap<String, Object> playerBids) {
+        this.playerBids = playerBids;
+    }
+
+    public void setLocationID(String locationID) {
+        this.locationID = locationID;
     }
 
     //TODO: create functions that handle bidding results and receiving property
