4c2a729b0beffa5ea7308aecf45a3dbff02dc201
VinSe <vincentmcmaps@gmail.com>
2021-06-19 19:55:17 +0200

    Added simple template for the unit tests classes

---
+++ src/main/java/Tests/templateTest.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+
+public class templateTest {
+
+    @Test
+    public void Should_doeIets_WhenIetsWordtGedaan() {
+        //Arrange
+
+
+        //Act
+
+
+        //Assert
+
+
+    }
+
+}
