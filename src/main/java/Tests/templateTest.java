0fbfd48b4b3a7daa3d1261055de2b4365eb58e9f
VinSe <vincentmcmaps@gmail.com>
2021-06-19 19:55:53 +0200

    Renaming

--- src/main/java/Tests/templateTest.java
+++ src/main/java/Tests/templateTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class templateTest {
 
     @Test
-    public void Should_doeIets_WhenIetsWordtGedaan() {
+    public void Should_doeIets_When_IetsWordtGedaan() {
         //Arrange
 
 
