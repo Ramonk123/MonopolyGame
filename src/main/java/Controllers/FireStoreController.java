eba060d8a80ac0996401f3d11f03fcd6c196d9b9
VinSe <vincentmcmaps@gmail.com>
2021-06-20 17:14:20 +0200

    Added locations to firebase

--- src/main/java/Controllers/FireStoreController.java
+++ src/main/java/Controllers/FireStoreController.java
@@ -101,13 +101,14 @@ public class FireStoreController implements Subject<DocumentSnapshot>, HasStage,
         PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
         TurnController turnController = (TurnController) ControllerRegistry.get(TurnController.class);
         CardDeckController cardDeckController = (CardDeckController) ControllerRegistry.get(CardDeckController.class);
+        LocationController locationController = (LocationController) ControllerRegistry.get(LocationController.class);
 
         Map<String, Object> lobbyData = (Map<String, Object>) ((LobbyController) ControllerRegistry.get(LobbyController.class)).getFirestoreFormat();
         lobbyData.put("players", playerController.getFirestoreFormat());
         lobbyData.put("turn", turnController.getFirestoreFormat());
         lobbyData.put("nextChanceCard", cardDeckController.getNextChanceCard());
         lobbyData.put("nextCommonFundCard", cardDeckController.getNextCommonFundCard());
-
+        lobbyData.put("locations", locationController.getLocationArray());
 
         com.google.cloud.firestore.Firestore database = firestore.getDatabase();
         ApiFuture<WriteResult> upload = database.collection("Lobbies").document(String.valueOf(token)).set(lobbyData);
