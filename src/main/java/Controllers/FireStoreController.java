635b04074fe6b051fcae861ea5b0f83e6d338661
Brandon <brrandonplokker@live.nl>
2021-06-20 04:51:00 +0200

    Removed bs

--- src/main/java/Controllers/FireStoreController.java
+++ src/main/java/Controllers/FireStoreController.java
@@ -98,10 +98,6 @@ public class FireStoreController implements Subject<DocumentSnapshot>, HasStage,
     }
 
     public void createLobby(int token) {
-
-        //TODO:
-        // add the real data instead of sample.
-        //    Subdirectories may be helpful for locations and players. Data structure still needs to be designed.
         PlayerController playerController = (PlayerController) ControllerRegistry.get(PlayerController.class);
         TurnController turnController = (TurnController) ControllerRegistry.get(TurnController.class);
         CardDeckController cardDeckController = (CardDeckController) ControllerRegistry.get(CardDeckController.class);
