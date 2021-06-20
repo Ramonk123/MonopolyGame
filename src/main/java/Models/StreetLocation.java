dfb45c4083f22ed13dffc9477b03c7531c54f21b
Brandon <brrandonplokker@live.nl>
2021-06-20 13:31:53 +0200

    current player instead of player1

--- src/main/java/Models/StreetLocation.java
+++ src/main/java/Models/StreetLocation.java
@@ -1,7 +1,6 @@
 package Models;
 
-import Controllers.ControllerRegistry;
-import Controllers.LocationController;
+import Controllers.*;
 import Monopoly.UUID;
 
 import java.util.List;
@@ -34,19 +33,23 @@ public class StreetLocation extends OwnableLocation {
 
     @Override
     public void action(Player player) {
+        TurnController turnController = (TurnController) ControllerRegistry.get(TurnController.class);
+        Players players = turnController.getCurrentPlayer();
+        PlayerController playerController= (PlayerController) ControllerRegistry.get(PlayerController.class);
+        Player currentPlayer = playerController.getPlayerByPlayersEnum(players).orElseThrow();
         System.out.println(getOwner().isEmpty());
         System.out.println("begin van action");
         LocationController locationController = (LocationController) ControllerRegistry.get(LocationController.class);
         //OwnableLocation location = (OwnableLocation) locationController.getLocationByEnum(this.getId()).orElseThrow();
             if (getOwner().isEmpty()){
                 System.out.println("streetlocation popup");
-                Actions.buyLocationPopup(player, getPrice(), this);
+                Actions.buyLocationPopup(currentPlayer, getPrice(), this);
             } else {
-                if(UUID.compare(getOwner().orElseThrow(), player)) {
+                if(UUID.compare(getOwner().orElseThrow(), currentPlayer)) {
                     //TODO:
                     // Is owned by the player standing on the location
                 } else {
-                    Actions.payFunds(player, getRent()* priceInflator.inflateByTicks(getHouses()));
+                    Actions.payFunds(currentPlayer, getRent()* priceInflator.inflateByTicks(getHouses()));
                     Actions.receiveFunds(getOwner().orElseThrow(), getRent()* priceInflator.inflateByTicks(getHouses()));
                 }
             }
